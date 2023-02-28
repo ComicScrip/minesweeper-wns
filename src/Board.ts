963eb21bcb96489d4042c0261530beee8accd9b7
Pierre Genthon <genthon.pierre@gmail.com>
2023-02-28 09:40:59 +0100

    board functions

--- src/Board.ts
+++ src/Board.ts
@@ -1,13 +1,24 @@
-export const forEachCell = (board, fn) => {};
+interface Cell {
+  x: number;
+  y: number;
+  val: number | "bomb";
+  revealed: boolean;
+}
 
-export const getNeighbors = (board, cell) => {};
+type Board = Cell[][];
 
-export const populateWithBombs = (board, bombRatio = 0.2) => {};
+export const createEmptyBoard = (size: number): Board => {};
 
-export const populateWithNeighborsCount = (board, bombRatio = 0.2) => {};
+export const forEachCell = (board: Board, fn: (cell: Cell) => void) => {};
 
-export const createEmptyBoard = (size) => {};
+export const getNeighbors = (board: Board, cell: Cell): Cell[] => {};
 
-export const getGameStatus = (board) => {};
+export const populateWithBombs = (board: Board, bombRatio = 0.2) => {};
+
+export const populateWithNeighborsCount = (board: Board) => {};
+
+export const getGameStatus = (
+  board: Board
+): "won" | "lost" | "inProgress" => {};
 
 export const createBoard = (size, bombRatio) => {};
