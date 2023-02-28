2cf4c1026bd43b5c9434187c1a667e596d7b8262
Pierre Genthon <genthon.pierre@gmail.com>
2023-02-28 08:42:23 +0100

    board functions

---
+++ src/Board.ts
@@ -0,0 +1,13 @@
+export const forEachCell = (board, fn) => {};
+
+export const getNeighbors = (board, cell) => {};
+
+export const populateWithBombs = (board, bombRatio = 0.2) => {};
+
+export const populateWithNeighborsCount = (board, bombRatio = 0.2) => {};
+
+export const createEmptyBoard = (size) => {};
+
+export const getGameStatus = (board) => {};
+
+export const createBoard = (size, bombRatio) => {};
